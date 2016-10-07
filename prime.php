f14f54269b7b7c47157658cac9f5c525d67c8770
sufiyan1122 <sufiyan11221122@gmail.com>
2016-10-07 09:37:37 +0530

    v2.0

--- prime.php
+++ prime.php
@@ -2,16 +2,9 @@
 if($_POST){
 	$num1=$_POST["num1"];
 	$num2=$_POST["num2"];
-	for($i=$num1;$i<=$num2;$i++)
-	{
-		for($j=0;$j<=$num1;$j++){
-		if($i%$j=0){
-			if($i==$j){
-				echo "$i is a prime no<br>";}
-		}
+	for($i=$num1;$i<=$num2;$i++){
+		
 	}
-}
-}
 else{
 	echo "enter the numbers";
 }
